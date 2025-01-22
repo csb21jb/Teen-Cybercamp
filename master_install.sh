f848014d1bd945c1fb4664349b31fe3604a25d0a
csb21jb <94072917+csb21jb@users.noreply.github.com>
2025-01-22 09:21:55 -0500

    Update master_install.sh

--- master_install.sh
+++ master_install.sh
@@ -30,8 +30,8 @@ apt install -y ca-certificates
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-sudo apt install docker-compose-plugin docker-ce
+apt update
+apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 
 # Configure the "testuser" account
