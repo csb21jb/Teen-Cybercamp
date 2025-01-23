e8a3ad641b61d750133e3c7950a3e6e1a8be9006
csb21jb <94072917+csb21jb@users.noreply.github.com>
2025-01-22 19:06:02 -0500

    Update master_install.sh

--- master_install.sh
+++ master_install.sh
@@ -35,10 +35,9 @@ sleep 2  # Wait for 2 seconds
 echo -e "\n\n\n\n"  # Add two blank lines for spacing
 # Setup Docker
 echo -e "\e[33mSetting up Docker...\e[0m"
-apt install -y ca-certificates 
 mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt update
 apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 # Ensure Docker service is running
